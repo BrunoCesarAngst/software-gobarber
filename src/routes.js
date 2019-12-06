c82b074a99d9b6e4a9b479e7da14add96e8aafe7
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-12-06 01:31:38 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Bruno César Angst',
-    email: 'bruno.angst@rede.ulbra.br',
-    password_hash: '12345671234567'
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
